81898a06564d383650141b26ef11297b2c519eb2
julientaq <julien@lesvoisinsdustudio.ch>
2025-07-21 11:24:45 +0200

    copy cname

--- eleventy.config.js
+++ eleventy.config.js
@@ -22,6 +22,7 @@ export default async function (eleventyConfig) {
   // filterTagList(tags , ["nav","posts"])
 
   eleventyConfig.addPassthroughCopy({ "static/css": "/css" });
+  eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy({ "static/fonts": "/fonts" });
   eleventyConfig.addPassthroughCopy({ "static/js": "/js" });
   eleventyConfig.addPassthroughCopy({ "static/images": "/images" });
